8c8b0bbaa2b0967e3cfc5bd1118a7ffbd212d014
Caio-Marinho <kaigabriel12@gmail.com>
2025-07-02 14:06:49 -0300

    opa

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from Estoque import Estoque
 
 estoque = Estoque()
 
-estoque.adicionar_produto("Notebook", 3500, 2)
+estoque.adicionar_produto("Notebook Samsung Book", 3500, 2)
 estoque.adicionar_produto("Notebook", 3500, 3)
 estoque.adicionar_produto("Celular", 2500)
 estoque.adicionar_produto("Celular", 2500,4)
 for item in estoque.listar():
     print(item)
-estoque.remover_produto("Notebook", 6)
+estoque.remover_produto("Notebook Samsung Book", 1)
+estoque.atualizar_nome("Notebook Samsung Book","Notebook Samsung Galaxy Book")
 for item in estoque.listar():
     print(item)
 estoque.remover_produto("Notebook", 5)
